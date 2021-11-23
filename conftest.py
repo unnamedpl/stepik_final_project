c71ca70edf48b009170b467db205b631d5dce335
unnamedpl <Q28133128q>
2021-11-23 10:28:20 +0200

    Added files "conftest.py" and "requirements.txt"

---
+++ conftest.py
@@ -0,0 +1,26 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome', help='Browser: Chrome or Firefox')
+    parser.addoption('--language', action='store', default='en', help='Language: en, ru ...')
+
+
+@pytest.fixture(scope='function')
+def browser(request):
+    browser_name = request.config.getoption('browser_name')
+    user_language = request.config.getoption('language')
+    if (browser_name == 'chrome'):
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+    elif (browser_name == 'firefox'):
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        print('Browser is non implemented')
+    yield browser
+    browser.quit()
