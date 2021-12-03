fde75b992be70d71c922cd59e4d5e4b8405fdc30
unnamedpl <Q28133128q>
2021-12-03 11:29:31 +0200

    Added marking for test with parameterization

--- test_product_page.py
+++ test_product_page.py
@@ -34,6 +34,7 @@ class TestUserAddToBasketFromProductPage:
         page.should_be_correct_price_in_basket()
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
                                   pytest.param(7, marks=pytest.mark.xfail),
                                   8, 9])
